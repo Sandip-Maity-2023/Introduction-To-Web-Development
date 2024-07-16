116ac4d9071505ecb203fa6ec0310a9c81e27a20
Sandip-Maity-2023 <12sandip125@gmail.com>
2024-07-16 06:55:55 +0530

    tutorials

--- 018 musketeers of web dev.js
+++ 018 musketeers of web dev.js
@@ -1,2 +1,25 @@
 console.log("hello");
-alert("apna college");
+//alert("apna college");
+console.dir(document.body);
+//document.body.childNodes[3].innerText="abcd";
+let heading=document.getElementById("header");
+console.dir("heading");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
