a840f3706b6f650e5bd26a62401a28d531f1f39a
Sandip-Maity-2023 <12sandip125@gmail.com>
2024-12-16 21:50:16 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,36 @@
-const express = require('express')
-const app = express()
-const port = 4001
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
 
-app.get('/', (req, res) => {
-  res.send('Hello Sandip!')
-})
+import bookRoute from "./route/book.route.js";
+import userRoute from "./route/user.route.js";
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+const URI = process.env.MongoDBURI;
+
+// connect to mongoDB
+try {
+    mongoose.connect(URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    console.log("Connected to mongoDB");
+} catch (error) {
+    console.log("Error: ", error);
+}
+
+// defining routes
+app.use("/book", bookRoute);
+app.use("/user", userRoute);
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -36,19 +36,31 @@ function App() {
 export default App
 */
 import React from "react";
-import Navbar from "./components/Navbar";
+import Home from "./home/Home";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Courses from "./courses/Courses";
+import Signup from "./components/Signup";
+import { Toaster } from "react-hot-toast";
+import { useAuth } from "./context/AuthProvider";
 
-function App(){
+function App() {
+  const [authUser, setAuthUser] = useAuth();
+  console.log(authUser);
   return (
-  <>
-
-  {/* <div className="text-xl font-semibold bg-red-700 cour">hello sandip */}
-  <Navbar/>
-  </> 
-  ); 
+    <>
+      <div className="dark:bg-slate-900 dark:text-white">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/course"
+            element={authUser ? <Courses /> : <Navigate to="/signup" />}
+          />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+        <Toaster />
+      </div>
+    </>
+  );
 }
+
 export default App;
-
-
-{/* className="text-3xl font-bold underline 
-*/}
