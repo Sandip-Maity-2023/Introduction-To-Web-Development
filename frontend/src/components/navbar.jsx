02da1b7742808b553dbcdee58209d2eee58d3353
Sandip-Maity-2023 <12sandip125@gmail.com>
2024-12-16 22:13:36 +0530

    update

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
+import Navbar from "./components/Navbar";
+/////
 import Login from "./Login";
 import Logout from "./Logout";
 import { useAuth } from "../context/AuthProvider";
