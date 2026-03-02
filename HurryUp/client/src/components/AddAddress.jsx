b45bb72c8d407e67f56b6f41b40029b7d48123ee
Sandip-Maity-2023 <12sandip125@gmail.com>
2026-03-02 13:18:28 +0530

    up

--- HurryUp/client/src/components/AddAddress.jsx
+++ HurryUp/client/src/components/AddAddress.jsx
@@ -1,32 +1,47 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import SummaryApi from "../common/SummaryApi";
+import toast from "react-hot-toast";
+import AxiosToastError from "../utils/AxiosToastError";
+import { IoClose } from "react-icons/io5";
+import { useGlobalContext } from "../provider/GlobalProvider";
 
-import React from 'react'
 
-const AddAddress=({close}) =>{
+const AddAddress = ({ close }) => {
+  const { register, handleSubmit, reset } = useForm();
+  const { fetchAddress } = useGlobalContext();
 
-    const {register,handleSubmit,reset}=useForm()
-    const {fetchAddress}=useGlobalContext()
+  const onSubmit = async (data) => {
+    console.log("data", data);
 
-    const onSubmit=async(data)=>{
-        console.log("data",data)
+    try {
+      const response = await Axios({
+        ...SummaryApi.createAddress,
+        data: {
+          address_line: data.address_line,
+          city: data.city,
+          state: data.state,
+          country: data.country,
+          pincode: data.pincode,
+          mobile: data.mobile,
+        },
+      });
 
-        try {
-            
-        } catch (error) {
-            
+      const { data: responseData } = response;
+
+      if (responseData.success) {
+        toast.success(responseData.message);
+        if (close) {
+          close();
+          reset();
+          fetchAddress();
         }
+      }
+    } catch (err) {
+      AxiosToastError(err);
     }
-  return (
-    <div>AddAddress</div>
-  )
-}
+  };
+  return <></>;
+};
 
-export default AddAddress
-
-
-
-
-
-
-
-
-
+export default AddAddress;
