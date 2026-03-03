306fc570d8af2e5b22a20a52443566d999f2727c
Sandip-Maity-2023 <12sandip125@gmail.com>
2026-03-03 20:20:41 +0530

    update

--- HurryUp/client/src/layouts/Dashboard.jsx
+++ HurryUp/client/src/layouts/Dashboard.jsx
@@ -1,18 +1,28 @@
 import React from 'react'
+import UserMenu from '../components/UserMenu'
+import { Outlet } from 'react-router-dom'
 import { useSelector } from 'react-redux'
-import isAdmin from '../utils/isAdmin'
 
-const AdminPermision = ({children}) => {
-    const user = useSelector(state => state.user)
+const Dashboard = () => {
+  const user = useSelector(state => state.user)
 
-
+  console.log("user dashboard",user)
   return (
-    <>
-        {
-            isAdmin(user.role) ?  children : <p className='text-red-600 bg-red-100 p-4'>Do not have permission</p>
-        }
-    </>
+    <section className='bg-white'>
+        <div className='container mx-auto p-3 grid lg:grid-cols-[250px,1fr]  '>
+                {/**left for menu */}
+                <div className='py-4 sticky top-24 max-h-[calc(100vh-96px)] overflow-y-auto hidden lg:block border-r'>
+                    <UserMenu/>
+                </div>
+
+
+                {/**right for content */}
+                <div className='bg-white min-h-[75vh] '>
+                    <Outlet/>
+                </div>
+        </div>
+    </section>
   )
 }
 
-export default AdminPermision
+export default Dashboard
